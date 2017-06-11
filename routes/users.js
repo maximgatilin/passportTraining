c53a3f05d69da3d96f7826c22bc5b6b6dad6547e
Maxim <gatilin@teamidea.ru>
2017-06-11 20:34:02 +0300

    Create backend and frontend to register users

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
 // Register
 router.get('/register', (req, res) => {
@@ -10,5 +11,44 @@ router.get('/register', (req, res) => {
 router.get('/login', (req, res) => {
   res.render('login');
 });
+
+// Register
+router.post('/register', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+
+  // Validation
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    res.render('register', {
+      errors
+    })
+  } else {
+    const newUser = new User({
+      name,
+      email,
+      username,
+      password
+    });
+    User.createUser(newUser, (err, user)=> {
+      if (err) throw err;
+      console.log(user)
+    });
+    req.flash('success_msg', 'You are registered and can now login');
+    res.redirect('/users/login');
+  }
+});
+
 
 module.exports = router;
